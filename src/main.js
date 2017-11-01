d306783afc95f1cdbd74371017135f34435c7507
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-01 18:57:14 +0800

    增加轮播组件，增加基础样式

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import '@/common/normalize.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
