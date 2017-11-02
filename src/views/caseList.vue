6e85ad34c5858fc4a02a52ff92675fec1aaef09f
afacode <afacode@outlook.com>
2017-11-02 11:06:22 +0800

    设计师，案例更新

--- src/views/caseList.vue
+++ src/views/caseList.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-      caseList
+  <div class="caseList">
+    <div class="singleCase">
+      <div class="leftPic">
+        <img src="http://placehold.it/160x160" class="headImg">
+      </div>
+      <div class="detail">
+        <p class="title">四家大院门厅晚风香堂</p>
+        <p class="houseType">大户型</p>
+        <div class="desiner">
+          <img src="http://placehold.it/40x40" alt="">
+          <p class="desinerName">梁启超</p>
+          <p class="desinerRank">牛逼设计师</p>
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 <script>
--- src/views/guide.vue
+++ src/views/guide.vue
@@ -1,10 +1,44 @@
 <template>
-  <div>
-      guide
+  <div class="bg">
+      <div class="button" @click="goHome">
+      	进入设计IN
+      </div>
   </div>
 </template>
 <script>
 export default {
-  
+    data() {
+        return {
+        };
+    },
+    methods: {
+        goHome(){
+              this.$router.push({ path:'Index'});
+        }
+    }
 }
 </script>
+<style scoped>
+.bg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: url('http://ovfllimsi.bkt.clouddn.com/npc_bg_new@3x.png');
+    background-size: 100% 100%;
+}
+.button{
+	position: absolute;
+    bottom: 1rem;
+    left:50%;
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+　　-ms-transform: translate(-50%, -50%);
+　　-o-transform: translate(-50%, -50%);
+　　transform: translate(-50%, -50%);
+	height: .2rem;
+	width: 2rem;
+	line-height: .2rem;
+	text-align: center;
+}
+
+</style>
