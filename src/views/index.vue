1af61bbad5286946eaceb7d27161d82ba3658c8e
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-02 11:37:42 +0800

    轮播demo

--- src/views/index.vue
+++ src/views/index.vue
@@ -29,7 +29,7 @@ export default {
   data(){
     return {
       swiperOption: {
-  
+         autoplay: 3000,
           grabCursor: true,
           setWrapperSize: true,
           autoHeight: true,
