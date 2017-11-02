6e38cc400b61ecde79d6d51cf92c7884dda86d81
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-02 13:50:00 +0800

    左侧菜单栏

--- src/views/demo.vue
+++ src/views/demo.vue
@@ -3,7 +3,7 @@
       <div class="shadow"></div>
       <div class="nav">
           <ul>
-              <li>
+              <li class="active">
                   <router-link  to="/index">
                       <i><img src="static/images/home.png"></i>
                       <span>首页</span>
@@ -39,29 +39,57 @@
   </div>
 </template>
 <style scoped>
-.lefBar{
-    position: relative;
-    background: #fafafa;
+.lefBar {
+    width:100%;
     height: 100%;
+    position: relative;
+}
+.shadow {
+    position: absolute;
+    width:100%;
+    height: 100%;
+    opacity: .5;
+    z-index: 2;
+    background-color: #000;
+
 }
 .lefBar ul {
     margin:0;
-    padding:0;
+    padding:.16rem 0 0 0;
 }
 .lefBar ul,
 .lefBar li {
   list-style: none;
   
 }
-.lefBar {
-  width: 100%;
-  height: 100%;
-  position: relative;
+.lefBar li.active{
+    background: #eeeeee;
+}
+.lefBar .nav{
+       position: relative;
+       z-index:55;
+    width:66.718%;
+    position: relative;
+    background: #fafafa;
+    height: 100%;
 }
 .lefBar ul li i {
   width: 0.16rem;
   display: inline-block;
   vertical-align: middle;
+  margin-left:.14rem;
+}
+.lefBar ul li:nth-child(1){
+    margin-top:0;
+}
+.lefBar li:last-child{
+    padding-top: .05rem;
+    border-top:#eee 1px solid;
+}
+.lefBar li {
+    line-height: .33rem;
+    margin:.14rem 0;
+    
 }
 .lefBar ul li i img {
   width: 100%;
@@ -69,6 +97,10 @@
 .lefBar ul li i span {
   display: inline-block;
   vertical-align: middle;
+}
+.lefBar li  a {
+    color:#666;
+    text-decoration: none;
 }
 </style>
 
