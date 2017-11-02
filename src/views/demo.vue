a3468ec1e8ced8b964b99997989f42fdb1d7be68
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-02 13:09:29 +0800

    图片名称修改

--- src/views/demo.vue
+++ src/views/demo.vue
@@ -1,5 +1,10 @@
 <template>
-  
+  <div class="lefBar">
+      <div class="shadow"></div>
+      <div class="nav">
+           <router-view></router-view>
+      </div>
+  </div>
 </template>
 <script>
 export default {
