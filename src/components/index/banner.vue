930c7f316603d89fb62f57d143be53da322bac05
chengf <277714747@qq.com>
2017-11-08 10:11:25 +0800

    首页banner提交，案例列表设计师详情路由跳转配置

--- src/components/index/banner.vue
+++ src/components/index/banner.vue
@@ -22,10 +22,10 @@
     data() {
       return {
       	banner:[
-      		{link:"../../../static/images/banner.jpg",src:"http://www.baidu.com"},
-      		{link:"../../../static/images/banner.jpg",src:"http://www.bbboo.com"},
-      		{link:"../../../static/images/banner.jpg",src:"https://www.toutiao.com"},
-      		{link:"../../../static/images/banner.jpg",src:"http://www.baidu.com"}
+      		{link:"../../../static/images/banner_P1.jpg",src:"http://g.eqxiu.com/s/JSiNdILo"}//,
+//    		{link:"../../../static/images/banner.jpg",src:"http://www.bbboo.com"},
+//    		{link:"../../../static/images/banner.jpg",src:"https://www.toutiao.com"},
+//    		{link:"../../../static/images/banner.jpg",src:"http://www.baidu.com"}
       	],
         // NotNextTick is a component's own property, and if notNextTick is set to true, the component will not instantiate the swiper through NextTick, which means you can get the swiper object the first time (if you need to use the get swiper object to do what Things, then this property must be true)
         // notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
