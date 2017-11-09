7db3154da03ba9f957dfeb8dc99259c2ef92d53a
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-09 18:11:18 +0800

    修改

--- src/common/js/shareWx.js
+++ src/common/js/shareWx.js
@@ -1,8 +1,7 @@
 import wx from 'weixin-js-sdk'
 import Vue from 'vue'
 const getId= function (url){
-  var ajaxUrl=minWebConfig.wxShareAjaxUrl;
-  Vue.jsonp(ajaxUrl,{url: url||window.location.href.replace(location.hash, "")})
+  Vue.jsonp('https://app.wesetup.cn/weixin/getWXUrl',{url: url||window.location.href.replace(location.hash, "")})
     .then(function(res) {
        
       if (res.code != "200") {
@@ -45,7 +44,7 @@ const  shareReady=function(title,desc,link,imgUrl){
           link:link,
           title: title, // 分享标题
           desc: desc, // 分享描述
-          imgUrl: imgUrl||minWebConfig.qiniuImgUrl+'logo.png', // 分享图标
+          imgUrl: imgUrl||'http://ovfllimsi.bkt.clouddn.com/logo.png', // 分享图标
           success: function(success) {
               console.log(success);
             // 用户确认分享后执行的回调函数
@@ -58,7 +57,7 @@ const  shareReady=function(title,desc,link,imgUrl){
             link:link,
             title: title, // 分享标题
             desc: desc, // 分享描述
-            imgUrl: imgUrl||minWebConfig.qiniuImgUrl+'logo.png', // 分享图标
+            imgUrl: imgUrl||'http://ovfllimsi.bkt.clouddn.com/logo.png', // 分享图标
           success: function(success) {
              console.log(success);
             // 用户确认分享后执行的回调函数
