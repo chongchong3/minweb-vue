1aef8913a89e9082086feb2da7485042b248468d
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-10 17:09:55 +0800

    修改域名

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@
 window.minWebConfig={
     serverDomin:'https://app.wesetup.cn/', //服务的域名
     qiniuImgUrl:'https://img.wesetup.cn/', //七牛图片镜像路径前缀
-    currentDomin:'https://shejiin.net/' //当前域名
+    currentDomin:'www.shejiin.net/' //当前域名
 }
