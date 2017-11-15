15ddeaf713cbcfff97cee4dcba99214e2a07ffaf
afacode <afacode@outlook.com>
2017-11-16 01:44:52 +0800

    update

--- src/components/appointment.vue
+++ src/components/appointment.vue
@@ -71,7 +71,7 @@ export default {
                      })
                    
               }
-              miniSiteAppoints({"designer_uid":this.desiner.designer_uid,"user_id":user_id} ) //预约设计师
+              miniSiteAppoints({"designer_uid":_self.desiner.designer_uid,"user_id":user_id} ) //预约设计师
               .then(function(response){
                      if(response.data.code!=200){
                         return MessageBox('提示', '查询异常');
