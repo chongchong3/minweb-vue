ed7dd45bb0c9c3527f1d4cf4776a4d55192cbc12
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-17 15:47:38 +0800

    debuuger

--- src/views/desinerCaseDetails.vue
+++ src/views/desinerCaseDetails.vue
@@ -34,6 +34,7 @@ export default {
     vm = this;
     this.authorId=this.getCookie("wechat_id");
     if(!this.authorId){
+      console.log( window.location.href = "/minisite/login?originUrl="+encodeURI(process.env.BASE_API+'/#'+this.$route.fullPath),'pathTest')
             window.location.href = "/minisite/login?originUrl="+encodeURI(process.env.BASE_API+'/#'+this.$route.fullPath);
          return
     }  
