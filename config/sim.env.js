6d01b57906b1822ce074fb68b64eac896e19b74a
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-19 22:01:50 +0800

    sim

--- config/sim.env.js
+++ config/sim.env.js
@@ -5,5 +5,5 @@ module.exports = {
   // BASE_API: '"https://m.shejiin.net"',//接口api
   // SHARE_API: '"m.shejiin.net"', //分享接口
   QINIU_IMG:'"https://img.wesetup.cn/"',//七牛图片
-  qiyukf:'"a6caa2ba23e0c81f8ff2fd291db0113f.js"'
+  qiyukf:'"9d26aae940dc05e124af352aa9938321.js"'
 };
