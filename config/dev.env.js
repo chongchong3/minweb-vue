9aae292f1db2a9ad51f88bca25bb0276988736f3
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-19 19:51:33 +0800

    注释掉多余配置文件

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,8 +1,8 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"', //发布环境
-  BASE_API: '"http://shejiin.dev/"',//接口api
-  SHARE_API: '"http://app.shejiin.dev/"', //分享接口
+  // BASE_API: '"http://shejiin.dev/"',//接口api
+  // SHARE_API: '"http://app.shejiin.dev/"', //分享接口
   QINIU_IMG:'"https://img.wesetup.cn/"',//七牛图片
   qiyukf:'"a6caa2ba23e0c81f8ff2fd291db0113f.js"'
 }
