fb45cbed3cf3f87533fef338038fb0a00c46ce6c
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-20 20:44:51 +0800

    在线咨询

--- config/env.env.js
+++ config/env.env.js
@@ -2,7 +2,7 @@ module.exports = {
     NODE_ENV: '"production"',
     ENV_CONFIG: '"env"', //发布环境
     QINIU_IMG:'"https://img.wesetup.cn/"',//七牛图片
-    qiyukf:'"9d26aae940dc05e124af352aa9938321.js"'
+    qiyukf:'"a6caa2ba23e0c81f8ff2fd291db0113f.js"'
   }
 
   // a6caa2ba23e0c81f8ff2fd291db0113f
--- config/sim.env.js
+++ config/sim.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sim"', //发布环境
   QINIU_IMG:'"https://img.wesetup.cn/"',//七牛图片
-  qiyukf:'"9d26aae940dc05e124af352aa9938321.js"'
+  qiyukf:'"a6caa2ba23e0c81f8ff2fd291db0113f.js"'
 };
