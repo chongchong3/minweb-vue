cedb7076432c3ba4b2045622b496632e0c0be006
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-11-26 09:18:07 +0800

    caseList css

--- src/components/desiner/caseList.vue
+++ src/components/desiner/caseList.vue
@@ -6,17 +6,14 @@
 
             <swiper-slide v-if="caseList.list.length>0" v-for="(item,index) in caseList.list" :key="index">
                 <div class="imgWrap caseImgWrap">
-                    <img :src="item.cover_image+'?imageView2/5/w/640'">
-                </div>
-                <div class="explain">
+                    <img :src="item.cover_image+'?imageView2/5/w/640/h/1136'">
+                     <div class="explain">
                     <p class="tit">{{item.title}}</p>
                     <p class="houseName">{{item.house_type_name}}</p>
-                    <!-- <div class="degree">
-                        <label>设计师级别</label>
-                        <span class="level">{{level}}</span>
-                    </div> -->
-
+                   
                 </div>
+                </div>
+               
             </swiper-slide>
 
         </swiper>
@@ -25,148 +22,133 @@
     </div>
 </template>
 <style>
-  html, body {
-      position: relative;
-      height: 100%;
-    }
-    body {
-      background: #eee;
-      font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-      font-size: 14px;
-      color:#000;
-      margin: 0;
-      padding: 0;
-    }
-.swiper-picList {
-    margin-left:20px;
-    margin-top:.63rem;
-    height: 95%;
+html,
+body {
+  position: relative;
+  height: 100%;
 }
-.swiper-picList  .wrap {
-  
-      height: 100%;
+body {
+  background: #eee;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
+}
+.swiper-picList {
+  margin-left: 0.1rem;
+  margin-top: 0.7rem;
+  height: 100%;
+}
+.swiper-picList .wrap {
+  margin-bottom: 0.2rem;
+  height: 100%;
 }
 .swiper-picList .imgWrap {
-    height: 100%;
-    margin-right:40px;
-
-
-   
+  height: 100%;
+  margin-right: 0.2rem;
+  position: relative;
 }
 
 .swiper-picList .imgWrap img {
-    width: 100%;
-    height: 90%;
-    border-radius: .1rem;
-    ;
+  width: 100%;
+  border-radius: 0.1rem;
+  height: 76%;
 }
 
 .swiper-picList .explain {
-    position: absolute;
-    width: 90%;
-    bottom: 0;
-    background: #fff;
-    border-radius: .06rem;
-    color: #000;
-    text-align: center;
-    padding-bottom: .3rem;
+  position: absolute;
+  width: 92%;
+  left: 4%;
+  bottom: 1rem;
+  background: #fff;
+  border-radius: 0.06rem;
+  color: #000;
+  text-align: center;
+  padding:.2rem 0;
 }
 
 .swiper-picList .explain .degree {
-
 }
 
 .swiper-picList .explain .degree label {
-    opacity: .7;
-    font-size: 14px;
+  opacity: 0.7;
+  font-size: 14px;
 }
 
 .swiper-picList .explain .tit {
-    margin-top: .15rem;
-    font-size: .18rem;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+  margin-top: 0.15rem;
+  font-size: 0.18rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 
 .swiper-picList .explain .houseName {
-    margin-top: .2rem;
-    font-size: 14px;
-    opacity: .45;
-    font-weight: bold;
-    margin-bottom: .2rem;
+  margin-top: 0.2rem;
+  font-size: 14px;
+  opacity: 0.45;
+  font-weight: bold;
+  margin-bottom: 0.2rem;
 }
 
 .swiper-picList .explain .score {
-    width: 1rem;
-    display: inline-block;
-    margin-left: .07rem;
+  width: 1rem;
+  display: inline-block;
+  margin-left: 0.07rem;
 }
 
 .swiper-picList .explain .score img {
-    width: 100%;
+  width: 100%;
 }
 
 .swiper-picList .swiper-container .swiper-slide img {
-    width: 100%;
+  width: 100%;
 }
 
 .swiper-picList.swiper-container {
-    width: 100%;
+  width: 100%;
 }
 
-
-
 .swiper-picList .swiper-inner {
-    width: 100%;
+  width: 100%;
 }
 
 .swiper-picList .swiper-slide {
-    position: relative;
-     width: 100%;
-  
- 
-
+  position: relative;
+  width: 100%;
 }
 .level {
-    color: #5fa333;
+  color: #5fa333;
 }
 </style>
 <script>
 import whiteNav from "../detailNav/whiteNav";
 var vm = {},
-    _initia = 0;
+  _initia = 0;
 export default {
-    components: {  whiteNav },
-    props: ['caseList', 'level'],
-    name: 'caseName',
-    data() {
+  components: { whiteNav },
+  props: ["caseList", "level"],
+  name: "caseName",
+  data() {
+    return {
+      swiperOptionCase: {}
+    };
+  },
+  created() {
+    vm = this;
+    this.swiperOptionCase = {
+      initialSlide: vm.$route.query.caseId,
+      slidesPerView: "auto",
+      spaceBetween: -10,
+      onSlideChangeStart: swiper => {
+        this.$emit("goDetails", swiper, false);
+      }
+    };
+  },
 
-        return {
-            swiperOptionCase: {
-
-            }
-        }
-    },
-    created() {
-        vm = this;
-        this.swiperOptionCase = {
-            initialSlide: vm.$route.query.caseId,
-             slidesPerView: 'auto',
-            spaceBetween: -20,
-            onSlideChangeStart: swiper => {
-                  this.$emit('goDetails', swiper,false);
-
-
-            },
-
-        }
-    },
-
-    methods: {
-
-    }
-}
+  methods: {}
+};
 </script>
 
 
