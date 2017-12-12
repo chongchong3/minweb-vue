7f20bc0502f3eb6a2fc916b768b59442b9de2e10
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-12-12 11:10:36 +0800

    add caseDetails

---
+++ src/views/caseDetailsNew.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="caseDetails">
+      <div class="head">
+          <span class="icon back"></span>
+          <img src="./tatics/caseDetails/headBg.png" class="caseBg">
+          <div class="descript">
+              <div class="shadow"></div>
+              <p class="tit">大理知名名宿</p>
+              <p>新中式/300平米</p>
+          </div>
+      </div>
+      <div class="desiner">
+          <div>
+              <img src="https://assets.wesetup.cn/2017/11/13/1510563184160-lkc2jpzlsvbotoawbgnv78pvi.jpg?imageView2/2/w/100" class="head">
+              <span class="name">唐娟娟</span>
+          </div>
+          <p class="brief">设计理念:日本动画导演、实拍、企业家日本动画导演、实拍、企业家日本动画导演、实拍、企业家日本动画导演、实拍、企业家日本动画导演、实拍、企业家日本动画导演、实拍、企业家</p>
+      </div>
+      <ul class="caseList">
+          <li>
+              <img src="" alt="">
+          </li>
+      </ul>
+      <ul class="caseBigList">
+          <li>
+              <img src="" alt="">
+              <p>1980年，这是案例、首部电视剧、从而开启了他的导演生涯，这是案例、首部电视剧、从而开启了他的导演生涯，这是案例、首部电视剧、从而开启了他的导演生涯，这是案例、首部电视剧、从而开启了他的导演生涯</p>
+          </li>
+      </ul>
+      <div class="footer">
+          <div class="appoint">
+              <span class="icon"><img src="" ></span>
+              <span class="tit">预约设计师</span>
+          </div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
