ac2e6712d05f9d2b1ccf47d7020c785519aab422
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-12-13 18:03:51 +0800

    add appoint

--- src/views/caseDetailsNew.vue
+++ src/views/caseDetailsNew.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="caseDetails">
       <div class="head">
-          <span class="icon back"></span>
+          <span class="icon back" @click="goBack"><img src="static/images/caseDetails/back.png"></span>
           <img src="static/images/caseDetails/headBg.png" class="caseBg">
           <div class="descript">
               <div class="shadow"></div>
@@ -54,7 +54,7 @@
           </li>
       </ul>
       <div class="footer">
-          <div class="appoint">
+          <div class="appoint" @click="appoinmnet">
              <span class="icon"><img src="static/images/clock.png" alt=""></span>
               <span class="tit">预约设计师</span>
           </div>
@@ -67,6 +67,18 @@
 }
 .caseDetails .head {
   position: relative;
+}
+.caseDetails .head .icon {
+  position: absolute;
+  z-index:22;
+  left: 0.08rem;
+  top:.17rem;
+  width:.1rem;
+  height: .21rem;
+  display: inline-block;
+}
+.caseDetails .head .icon  img {
+  width:100%;
 }
 .caseDetails .caseBg {
   width: 100%;
@@ -136,7 +148,7 @@
 }
 .caseDetails .caseList {
 
-background-color: #eee;
+background-color: #F2F2F2;
 
 }
 .caseDetails .caseList  .wp{
@@ -178,17 +190,18 @@ background-color: #eee;
     bottom:0;
     width:100%;
     height: .6rem;
-    background: #eee;;
+    border-top:#C9C9C9 1px solid;
+    background: #fff;;
 }
 .appoint {
-    margin:.1rem .38rem;
-    background: #88C462;
-    border-radius: .2rem;
-    text-align: center;
-    color:#fff;
-    font-size:.16rem;
-        height: .4rem;
-        line-height: .4rem;
+  margin:.1rem .38rem;
+  background: #88C462;
+  border-radius: .2rem;
+  text-align: center;
+  color:#fff;
+  font-size:.16rem;
+  height: .4rem;
+  line-height: .4rem;
 
 }
 .appoint .icon {
@@ -212,20 +225,26 @@ background-color: #eee;
 import Vue from "vue";
 import "@/common/css/swiper.min.css";
 import VueAwesomeSwiper from "vue-awesome-swiper";
+import { MessageBox } from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import { miniSiteAppoints } from '@/api/appoints'; //预约设计师
+import { checkAppointsStatus } from '@/api/checkAppointsStatus';
+import { checkLoginStatus } from '@/api/CheckLoginStatus';//查询是否绑定
+
 
 Vue.use(VueAwesomeSwiper);
 
 export default {
   data() {
     return {
+       authorization_id:'',
       designerOption: {
         pagination: "null",
         slidesPerView: "auto",
         paginationClickable: true,
         onTransitionStart(swiper) {},
         onClick(swiper) {}
-        // more Swiper configs and callbacks...
-        // ...
+     
       }
     };
   },
@@ -233,6 +252,53 @@ export default {
     swiper() {
       return this.$refs.mySwiper.swiper;
     }
+  },
+  methods:{
+    goBack(){
+      window.history.back(); 
+    },
+     appoinmnet() {
+      var _self=this;
+    
+      //查询是否授权绑定用户
+        checkLoginStatus({authorization_id:this.desiner.authorId})
+        .then(function(response){
+            if(response.data.code!=200){
+                   return MessageBox('提示', '查询失败');
+            }
+         
+            if(!response.data.data.userId){ //如果没有绑定跳转登录页面
+                 return _self.$router.push({path:'/login?designer_uid='+_self.desiner.designer_uid})
+            }
+            var user_id = response.data.data.userId;
+            checkAppointsStatus({user_id: user_id})
+            .then(function(response){
+              
+              if( response.data.code==200 && response.data.data.message.length == 17 ){ //设计师ID长度为17
+                  return MessageBox('你已经预约过了');
+              }
+
+              miniSiteAppoints({"designer_uid":_self.desiner.designer_uid,"user_id":user_id} ) //预约设计师
+              .then(function(response){
+                    if(response.data.code==500){
+                        return MessageBox('提示', '你已经预约过了');
+                    } 
+                    if(response.data.code==200){
+                      return MessageBox('提示', '预约成功');
+                    }
+                    return MessageBox('提示', '查询异常')
+              })
+
+            })
+
+
+        })
+        .catch(function(error){
+        
+            return MessageBox('提示', '请求失败');
+        })
+    },
+ 
   }
 };
 </script>
