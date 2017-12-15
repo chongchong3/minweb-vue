29047e583a7c1691d75feab9a0b7868b4698fe2c
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-12-15 09:40:29 +0800

    自我介绍

--- src/components/desiner/self.vue
+++ src/components/desiner/self.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="self-wrap" class="slef-wrap">
-        <!-- <transparent-nav></transparent-nav> -->
+        <video-player x5-video-player-type="h5" x5-video-player-fullscreen="false" webkit-playsinline playsinline class="video-player-box vjs-big-play-centered hide" ref="videoPlayer" :options="playerOptions" @play="onPlayerPlay($event)" @pause="onPlayerPause($event)" @ended="onPlayerEnded($event)">
+        </video-player>
 
         <div class="slef-background hasTips" id="slef-background">
-            <img :src="selfMes.bodyImg" >
-       
+            <img :src="selfMes.bodyImg" @click="videoPlay">
+            <div class="playerButton">
+                <div class="playerBtn" @click="videoPlay">
+                    <!-- <img src="../../static/images/video.png"> -->
+                </div>
+
+            </div>
 
             <div class="memo">
                 <div class="cont">
-                    <p class="name">设计师说</p>
+                    <p class="name">个人信息</p>
 
-                    <p class="brief" v-html="selfMes.brief"></p>
+                    <p class="brief">{{selfMes.brief}}</p>
                 </div>
             </div>
         </div>
@@ -19,7 +25,6 @@
 </template>
 <style >
 .hasTips {
-    padding-top:.5rem;
     height: 100%;;
 }
 .imgWrap img {
@@ -45,7 +50,7 @@
 
     width: .6rem;
     margin: 0 0 0 .3rem;
-    background: url('../../../static/images/video.png');
+    background: url('../../static/images/video.png');
     background-size: 100% 100%;
     height: .6rem;
 }
@@ -93,7 +98,7 @@
 }
 
 .hasTips .memo .name {
-    font-size: 26px;
+    font-size: 30px;
 }
 
 .hasTips {
@@ -108,7 +113,7 @@
     text-align: left;
     z-index: 99;
     background: transparent;
-    background: url('../../../static/images/videoBg.png');
+    background: url('../../static/images/videoBg.png');
     background-size: 100% 100%;
 }
 .hasTips .score img {
@@ -116,6 +121,9 @@
 }
 </style>
 <script>
+
+import 'video.js/dist/video-js.css'
+
 export default {
     props: ['selfMes'],
     data() {
@@ -123,17 +131,49 @@ export default {
         return {
             case_id: 1,
 
+            playerOptions: {
+                // videojs options
+                muted: true,
+                sources: [{
+                    type: "video/mp4",
+                    src: 'http://os5lxzkas.bkt.clouddn.com/long15.m4v',
+                }],
+                poster: this.selfMes.full_body_shot_url,
+            }
+
+
+
         }
     },
 
   
     computed: {
 
+        player() {
+            return this.$refs.videoPlayer.player
+        }
     },
     methods: {
+
+        videoPlay: function(event) {
+            $('.slef-background').addClass('hide');
+            $('.video-player-box').removeClass('hide');
+            this.player.show();
+            this.player.play();
+        },
+
+        onPlayerEnded(playerCurrentState) {
+            this.videoHide();
+        },
+        
+        videoHide(){
+            $('.slef-background').removeClass('hide');
+            $('.video-player-box').addClass('hide');
+            this.player.pause();
+        }
+
 
 
     }
 }
 </script>
-
