ce90d5984f94a592c1d81830b66ef22f984aa6fe
chengf <277714747@qq.com>
2017-12-18 21:12:48 +0800

    调优

--- src/views/desinerDetails.vue
+++ src/views/desinerDetails.vue
@@ -3,7 +3,8 @@
 		<div class="page-swiper " v-touch:swipeup="up" v-touch:swipedown="down">
 			<div isScroll="true" @click="back" class="goback"></div>
 	        <div class="por-des-c" :style="{backgroundImage: 'url(' + result.background_img + '?imageMogr2/auto-orient/interlace/1/blur/26x10/quality/85|imageslim)'}" >
-		        <div id="portrait" :style="{backgroundImage:'url(' + result.full_body_shot_url   + ') '}" class="portrait-c" >
+	        	
+		        <!--<div id="portrait" :style="{backgroundImage:'url(' + result.full_body_shot_url   + ') '}" class="portrait-c" >
 		        </div>
 		        
 		        <div class="designer-info-c">
@@ -12,13 +13,29 @@
 		        		报价：{{result.designer_price}}-{{result.designer_high_price}}元/平米
 		        	</span></p>
 		        		
+		        </div>-->
+		         
+		        <div style="position: relative;width: 100%;height:100%">
+			        <div id="portrait" :style="{backgroundImage:'url(' + result.full_body_shot_url   + ') '}" class="portrait-c" >
+			        </div>
+			        
+			        <div class="designer-info-c">
+			        	<p class="name">{{result.designer_name}}<br>
+			        	<span class="price">
+			        		报价：{{result.designer_price}}-{{result.designer_high_price}}元/平米
+			        	</span></p>
+			        		
+			        </div>
 		        </div>
+		        
+		        
+		        
 	        </div>
 	        <div class="up-icon"></div>
 	        <div class="detail-describe-c">
 	        	<div class="detail-describe-sub-c">
-	        		<p class="title">奖项</p>  
-	        		<p class="content">{{result.awards}}</p>
+	        		<p class="title" v-if="!result.awards == ''">奖项</p>  
+	        		<p class="content" v-if="!result.awards == ''">{{result.awards}}</p>
 	        		<p class="title">设计理念</p>
 	        		<p class="content">{{result.plantform_descript}}</p>
 	        		<p class="title">个人简介</p>  
@@ -93,7 +110,7 @@
 		height:100%;
 		margin:0px auto;
 		position: absolute;
-		background-size: 100%;
+		background-size: cover;
 		background-repeat: no-repeat;
 	}
 	.portrait-c img{
@@ -102,7 +119,7 @@
 	.designer-info-c{
 		width:100%;
 		position:absolute;
-		bottom:.4rem;;
+		bottom:.4rem;
 		left:0px;
 		z-index: 11;
 	}
@@ -112,7 +129,7 @@
 		padding:0;
 		margin:0px;
 		padding-left:3.2%;
-		display: inline-block;
+		/*display: inline-block;*/
 	}
 	.text-center{
 		text-align: center;
@@ -125,6 +142,8 @@
 	.designer-info-c .price{
 		font-size:.12rem;
 		line-height:.17rem;
+		word-break: keep-all;
+		white-space: nowrap;
 	}
 	
 	.up-icon{
@@ -258,7 +277,7 @@
 			if(navigator.userAgent.toLowerCase().indexOf("android")>-1){
 				document.getElementById('caselist-c').addEventListener('scroll', function(e){
 					e.preventDefault();
-					self.down();
+					//self.down();
 					}, false);
 			}
 		},
@@ -354,8 +373,9 @@
 					'font-size':'.16rem'
 				})
 				$(".designer-info-c .name").animate({
-					"padding-left":($(window).width()-wth)/2,
-					"text-align":"center"
+//					"padding-left":($(window).width()-wth)/2,
+					"text-align":"center",
+					"paddingLeft":0
 				}).css("textAlign","center")
 				
 				$(".detail-describe-c").animate({
@@ -526,15 +546,15 @@
 				$(".up-icon").animate({
 					'bottom':"0",
 				})
-				var wth = $($(".designer-info-c p")[0]).width();
+				
 				$(".price").animate({
 					'font-size':'.16rem'
 				})
-				$(".designer-info-c .name").animate({
-					"padding-left":($(window).width()-wth)/2,
-					"text-align":"center"
-				}).css("textAlign","center")
 				
+				$(".designer-info-c .name").animate({
+					"text-align":"center",
+					"paddingLeft":0
+				}).css("textAlign","center")
 				$(".detail-describe-c").animate({
 					"bottom":"0",
 					"height":"60%"
