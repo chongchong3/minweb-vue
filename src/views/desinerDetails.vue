460ca27d16ecac9173510162cac022c52725aef4
chengf <277714747@qq.com>
2017-12-19 14:05:27 +0800

    滑动调试

--- src/views/desinerDetails.vue
+++ src/views/desinerDetails.vue
@@ -119,7 +119,7 @@
 	.designer-info-c{
 		width:100%;
 		position:absolute;
-		bottom:.4rem;
+		bottom:.3rem;
 		left:0px;
 		z-index: 11;
 	}
@@ -448,7 +448,7 @@
 								
 							}
 							
-						},10)
+						},30)
 		            	
 
 						
@@ -456,11 +456,26 @@
 				}else{
 					if(this.step == 1){
 						this.animateDownwithoutVideo();
-					}else if(this.step == 2 && $('.caselist-c').scrollTop() <=0){//&& $('body').scrollTop() == 0
-					
-	//					alert($('body').scrollTop())
-						this.hideListwithoutVideo();
-//						$(".caselist-c").css("overflow","hidden");
+					}else if(this.step == 2){//&& $('body').scrollTop() == 0
+						var self= this;
+						var caselist = document.getElementById("caselist-c");
+						
+						var scrollTopStart = caselist.scrollTop;
+						var scrollTopEnd = "";
+						var timer = setInterval(function(){
+							scrollTopEnd = caselist.scrollTop;
+							if(scrollTopEnd<scrollTopStart){
+								scrollTopStart = scrollTopEnd;
+							}else{
+								clearInterval(timer);
+								if(scrollTopEnd<=0){
+									this.hideListwithoutVideo();
+								}
+								
+							}
+							
+						},10)
+						
 					}
 				}
 			},
@@ -485,7 +500,9 @@
 				$(".video-c").animate({
 					'bottom':"0",
 				})
-//				$(".vjs-tech")[0].play();
+				if($(".vjs-tech")[0].paused){
+					$(".vjs-tech")[0].play();
+				}
 //				setTimeout(function(){
 //					$(".desinerDetails").css("overflow","visible");
 //				},100)
@@ -513,7 +530,7 @@
 						'bottom':bottom
 					})
 				}
-//				$(".vjs-tech")[0].pause();
+				$(".vjs-tech")[0].pause();
 				
 				
 			},
