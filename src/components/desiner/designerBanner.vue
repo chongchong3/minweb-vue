d94874bfb3f70c4482267e355e3e5cdafce58ac8
chengf <277714747@qq.com>
2017-12-21 15:01:47 +0800

    详情顶部banner，打印日志代码去除

---
+++ src/components/desiner/designerBanner.vue
@@ -0,0 +1,96 @@
+<template>
+	<div>
+	<div class="designer-banner">
+		  <swiper :options="swiperOption" class="contain-visible">
+		    <swiper-slide class="swiper-item" v-for="(item,index) in imgItems" :key="index">
+		    	<img :src="item"  />
+		    </swiper-slide>
+		    <div class="swiper-pagination"  slot="pagination"></div>
+		  </swiper>
+	</div>
+
+	</div>
+</template>
+<script>
+  // swiper options example:
+  export default {
+    props: ["imgItems"],
+    data() {
+      return {
+      	
+        swiperOption: {
+          grabCursor: true,
+          setWrapperSize: true,
+          autoHeight: true,
+          pagination: '.swiper-pagination',
+          paginationType:'bullets',
+          paginationClickable: true,
+          mousewheelControl: true,
+          observeParents: true
+        }
+      }
+    },
+   
+    computed: {
+
+    },
+    mounted() {
+
+    }
+  }
+</script>
+<style >
+	.designer-banner {
+		width: 100%;
+		background:#fff;
+	}
+	.designer-banner:after{
+		height: 0rem;
+		background: #fff;
+		display: block;
+		content: " ";
+	}
+	.banner-hor-c{
+		background:#F4F4F4;
+		height:.1rem;
+	}
+	.swiper-container {
+		overflow: hidden;
+		position: relative;
+	}
+
+	.designer-banner .swiper-wrapper{
+		height:100%;
+		overflow: hidden;
+	}
+	
+  .swiper-item {
+    height: 100%;
+    float: left;
+  }
+  .swiper-item img{
+  	display: block;
+  	width: 100%;
+  }
+  .swiper-pagination{
+  	height:0.2rem;
+  	width:100%;
+  	position: absolute;
+  	bottom:10px;
+  	left:0px;
+  	text-align: right;
+  }
+  .swiper-pagination-bullet{
+  	opacity: .6;
+  	background:#fff;
+  	display: inline-block;
+  	margin: 0px 0.04rem;
+  	width:0.06rem;
+  	height:.06rem;
+  	border-radius: 50%;
+  }
+  .swiper-pagination-bullet-active{
+  	background:#fff;
+  	opacity: 1;
+  }
+</style>
