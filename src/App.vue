f50145a496813372fb829ea79c3e2a225ab2b6fd
afacode <afacode@outlook.com>
2018-01-02 14:52:38 +0800

    页面缓存

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view v-wechat-title="$route.meta.title"></router-view> 
+    <keep-alive>
+      <router-view v-wechat-title="$route.meta.title" v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+     <router-view v-wechat-title="$route.meta.title" v-if="!$route.meta.keepAlive"></router-view>
   </div>
 </template>
 
